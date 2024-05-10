df0ee8a5d6573543e6ed6a30e4e5d85b63d6ac81
patb-github <165882561+patb-github@users.noreply.github.com>
2024-05-10 14:12:57 +0700

    Add examples for let variables

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// let variable
+// Block Scope 1 - let inside of block
+{
+    let x = 1;
+    console.log(x);
+}
+// console.log(x); // Error: x is not defined
 
+// Block Scope 2 - reassignment of global variable through block scope
+let y = 2;
+{
+    y = 3;
+    console.log(y);   // logs 3
+}
+console.log(y);       // logs 3
+
+// Block Scope 3 - 'z' declared in block shadows global variable in block scope 
+let z = 8;
+{
+    let z = 10;
+    console.log(z);   // logs 10
+}
+console.log(z);       // logs 8
+
+// Function Scope
+function my_stock() {
+    let my_stock = 0;       // shadows my_stock function
+    console.log(my_stock);  // logs 0
+}
+
+console.log(my_stock);      // logs [Function: my_stock]
 
 
